751aec75962c958234bd3cd9327c3f0bbbbc8cf3
Johnny Freeman <prsjohnny@gmail.com>
2019-12-01 21:53:51 -0500

    fixed mobile styling for menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,8 +1,8 @@
 @include('input', ['input' => 'menu'])
 
-<ul class="font-sans mt-2">
-    <li class="inline"><a class="uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-2" href="{{ url('about') }}" data-action="click->terminal#execute" data-terminal-input="about">about</a></li>
-    <li class="inline"><a class="uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-2" href="{{ url('blog') }}" data-action="click->terminal#execute" data-terminal-input="blog">blog</a></li>
-    <li class="inline"><a class="uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-2" href="{{ url('projects') }}" data-action="click->terminal#execute" data-terminal-input="projects">projects</a></li>
-    <li class="inline"><a class="uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-2" href="{{ url('contact') }}" data-action="click->terminal#execute" data-terminal-input="contact">contact</a></li>
+<ul class="flex flex-wrap font-sans">
+    <li class="inline mt-2 mr-2"><a class="inline-block uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-1" href="{{ url('about') }}" data-action="click->terminal#execute" data-terminal-input="about">about</a></li>
+    <li class="inline mt-2 mr-2"><a class="inline-block uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-1" href="{{ url('blog') }}" data-action="click->terminal#execute" data-terminal-input="blog">blog</a></li>
+    <li class="inline mt-2 mr-2"><a class="inline-block uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-1" href="{{ url('projects') }}" data-action="click->terminal#execute" data-terminal-input="projects">projects</a></li>
+    <li class="inline mt-2 mr-2"><a class="inline-block uppercase tracking-wide bg-blue-400 rounded text-white hover:bg-blue-500 px-3 py-1" href="{{ url('contact') }}" data-action="click->terminal#execute" data-terminal-input="contact">contact</a></li>
 </ul>
