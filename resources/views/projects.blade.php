f66acb2c28a6f7f1df9b98e933a25cf5e063eeb2
Johnny Freeman <prsjohnny@gmail.com>
2019-12-03 19:16:08 -0500

    updated projects

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,3 +1,10 @@
 @include('input', ['input' => 'projects'])
 
-<p class="">// TODO: Use Github API to pull in select projects</p>
+<p class="">Here are some recent works of mine<span class="text-red-400">.</span></p>
+
+<div class="mt-6 md:flex w-full max-w-sm bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+    <div class="">
+        <div class=""><svg class="inline-block fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.13,9.48c.52-1.06,2.21-5.1-.86-7.61A.5.5,0,0,0,21,1.75C19,1.75,16.51,4.2,15.74,5A14.61,14.61,0,0,0,8.3,5C7.51,4.19,5,1.75,3.08,1.75a.5.5,0,0,0-.32.11C.12,4,.92,7.52,1.89,9.44a9.41,9.41,0,0,0-1.31,5.8c.26,3.31,3,7,6.48,7h9.89c3.5,0,6.23-3.69,6.48-7A9.4,9.4,0,0,0,22.13,9.48ZM16,19.75H8a3.5,3.5,0,0,1-3.5-3.5c0-5.91,4.13-3.53,7.5-3.53s7.5-2.44,7.5,3.53A3.5,3.5,0,0,1,16,19.75Z"/><ellipse cx="15.52" cy="16.25" rx="1" ry="1.5"/><ellipse cx="8.52" cy="16.25" rx="1" ry="1.5"/></svg> <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/johnfreeman.dev">johnfreeman.dev</a></div>
+        <p class="text-gray-600">The source code for my website.
+    </div>
+</div>
