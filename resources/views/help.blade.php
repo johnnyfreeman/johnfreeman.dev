a3657e7d69c64eda94a48e3c66e0c9ce97f4b51c
Johnny Freeman <prsjohnny@gmail.com>
2019-12-03 19:01:36 -0500

    hid the built-with command until i can flesh that out

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -3,10 +3,10 @@
 <p class="">My website is built like a command<span class="text-red-400">-</span>line application<span class="text-red-400">.</span> Here are a list of commands you can execute<span class="text-red-400">.</span> For your convenience<span class="text-red-400">,</span> you can also click around too<span class="text-red-400">.</span> :)</p>
 
 <table class="mt-8">
-    <tr>
+    {{-- <tr>
         <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1" href="{{ url('built-with') }}" data-action="click->terminal#execute" data-terminal-input="built-with">built-with</a></th>
         <td class="pl-4">Technologies used building this</td>
-    </tr>
+    </tr> --}}
     <tr>
         <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1" href="{{ url('clear') }}" data-action="click->terminal#execute" data-terminal-input="clear">clear</a></th>
         <td class="pl-4">Clear all output</td>
