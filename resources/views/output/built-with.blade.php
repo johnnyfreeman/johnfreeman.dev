1abccf337d3c3675037e03512ebaa35d573e930b
Johnny Freeman <prsjohnny@gmail.com>
2019-12-12 01:04:31 -0500

    added other projects

--- resources/views/output/built-with.blade.php
+++ resources/views/output/built-with.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @include('common.input', ['input' => 'projects/johnfreeman.dev'])
+    @include('common.input', ['input' => 'built-with'])
 
     <p class="w-full max-w-3xl">This isn't your traditional personal website with a few pages and a contact form.</p>
 </div>
