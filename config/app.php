c10bfe7a5d02828719be6ea73bf81742d20805ce
Johnny Freeman <prsjohnny@gmail.com>
2019-12-28 14:54:11 -0500

    updated config values

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'John Freeman'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://johnfreeman.dev'),
 
     'asset_url' => env('ASSET_URL', null),
 
