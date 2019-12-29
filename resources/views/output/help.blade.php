3d4d97e0de0a70faf436c84dc3eda301c9c026c7
Johnny Freeman <prsjohnny@gmail.com>
2019-12-29 16:00:07 -0500

    added missing commands to the help command

--- resources/views/output/help.blade.php
+++ resources/views/output/help.blade.php
@@ -7,6 +7,14 @@
 
     <table class="mt-2">
         <tr>
+            <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1" href="{{ url('about') }}" data-action="click->terminal#execute" data-terminal-input="about">about</a></th>
+            <td class="pl-4">A little about myself</td>
+        </tr>
+        <tr>
+            <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1" href="{{ url('blog') }}" data-action="click->terminal#execute" data-terminal-input="blog">blog</a></th>
+            <td class="pl-4">Articles I've written</td>
+        </tr>
+        <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1" href="{{ url('built-with') }}" data-action="click->terminal#execute" data-terminal-input="built-with">built-with</a></th>
             <td class="pl-4">Libraries and APIs used in building this</td>
         </tr>
@@ -15,12 +23,16 @@
             <td class="pl-4">Clear all output</td>
         </tr>
         <tr>
+            <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('contact') }}" data-action="click->terminal#execute" data-terminal-input="contact">contact</a></th>
+            <td class="pl-4">Get in touch with me</td>
+        </tr>
+        <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('features') }}" data-action="click->terminal#execute" data-terminal-input="features">features</a></th>
             <td class="pl-4">List of features</td>
         </tr>
         <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('help') }}" data-action="click->terminal#execute" data-terminal-input="help">help</a></th>
-            <td class="pl-4">This</td>
+            <td class="pl-4">You're looking at it</td>
         </tr>
         <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('intro') }}" data-action="click->terminal#execute" data-terminal-input="intro">intro</a></th>
@@ -29,6 +41,10 @@
         <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('menu') }}" data-action="click->terminal#execute" data-terminal-input="menu">menu</a></th>
             <td class="pl-4">Website menu</td>
+        </tr>
+        <tr>
+            <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('open-source') }}" data-action="click->terminal#execute" data-terminal-input="open-source">open-source</a></th>
+            <td class="pl-4">My open-source projects</td>
         </tr>
         <tr>
             <th class="text-left pt-1"><a class="bg-gray-900 text-teal-400 p-1 light:bg-gray-200 light:text-teal-600" href="{{ url('social') }}" data-action="click->terminal#execute" data-terminal-input="social">social</a></th>
