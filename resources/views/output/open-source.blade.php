3db413142f52ee8830b5f7ff817b99e992932adf
John Freeman <prsjohnny@gmail.com>
2021-04-19 02:06:48 -0400

    updated open source with archived repos

--- resources/views/output/open-source.blade.php
+++ resources/views/output/open-source.blade.php
@@ -1,52 +1,104 @@
 <div data-controller="output" data-terminal-target="output">
     @include('common.input', ['input' => 'open-source'])
 
-    <p class="">I enjoy giving back to the community<span class="text-red-400">.</span></p>
+    <p class="">When I'm able, I enjoy giving back to the community<span class="text-red-400">.</span></p>
 
-    <div class="mt-6 w-full max-w-2xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
-        <div class="flex items-center">
-            <svg class="inline-block fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.13,9.48c.52-1.06,2.21-5.1-.86-7.61A.5.5,0,0,0,21,1.75C19,1.75,16.51,4.2,15.74,5A14.61,14.61,0,0,0,8.3,5C7.51,4.19,5,1.75,3.08,1.75a.5.5,0,0,0-.32.11C.12,4,.92,7.52,1.89,9.44a9.41,9.41,0,0,0-1.31,5.8c.26,3.31,3,7,6.48,7h9.89c3.5,0,6.23-3.69,6.48-7A9.4,9.4,0,0,0,22.13,9.48ZM16,19.75H8a3.5,3.5,0,0,1-3.5-3.5c0-5.91,4.13-3.53,7.5-3.53s7.5-2.44,7.5,3.53A3.5,3.5,0,0,1,16,19.75Z"/><ellipse cx="15.52" cy="16.25" rx="1" ry="1.5"/><ellipse cx="8.52" cy="16.25" rx="1" ry="1.5"/></svg>
-            <a class="ml-2 text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/johnfreeman.dev">johnfreeman.dev</a>
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/johnfreeman.dev">johnfreeman.dev</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2020</span>
         </div>
         <p class="text-gray-700">The source code for my website</p>
-        <ul class="w-full list-none mt-3 flex flex-wrap items-center">
-            <li class="inline-flex items-center mr-4">
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-red-400 light:bg-red-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://github.com/laravel/laravel">laravel</a>
             </li>
-            <li class="inline-flex items-center mr-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-teal-400 light:bg-teal-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://github.com/tailwindcss/tailwindcss">tailwind</a>
             </li>
-            <li class="inline-flex items-center mr-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-yellow-400 light:bg-yellow-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://github.com/stimulusjs/stimulus">stimulus</a>
             </li>
-            <li class="inline-flex items-center mr-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-blue-400 light:bg-blue-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://github.com/axios/axios">axios</a>
             </li>
         </ul>
     </div>
 
-    <div class="mt-6 w-full max-w-2xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
-        <div class="flex items-center">
-            <svg class="inline-block fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.13,9.48c.52-1.06,2.21-5.1-.86-7.61A.5.5,0,0,0,21,1.75C19,1.75,16.51,4.2,15.74,5A14.61,14.61,0,0,0,8.3,5C7.51,4.19,5,1.75,3.08,1.75a.5.5,0,0,0-.32.11C.12,4,.92,7.52,1.89,9.44a9.41,9.41,0,0,0-1.31,5.8c.26,3.31,3,7,6.48,7h9.89c3.5,0,6.23-3.69,6.48-7A9.4,9.4,0,0,0,22.13,9.48ZM16,19.75H8a3.5,3.5,0,0,1-3.5-3.5c0-5.91,4.13-3.53,7.5-3.53s7.5-2.44,7.5,3.53A3.5,3.5,0,0,1,16,19.75Z"/><ellipse cx="15.52" cy="16.25" rx="1" ry="1.5"/><ellipse cx="8.52" cy="16.25" rx="1" ry="1.5"/></svg>
-            <a class="ml-2 text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/laravel-custom-relation">laravel-custom-relation</a>
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/laravel-model-config">laravel-model-config</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2019</span>
         </div>
-        <p class="text-gray-700">A custom relation for when stock relations aren't enough</p>
-        <ul class="w-full list-none mt-3 flex flex-wrap items-center">
-            <li class="inline-flex items-center mr-4">
+        <p class="text-gray-700">A config() for your laravel models</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-purple-400 light:bg-purple-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="http://php.net">php</a>
             </li>
         </ul>
     </div>
 
-    <div class="mt-6 w-full max-w-2xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
-        <div class="flex items-center">
-            <svg class="inline-block fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22.13,9.48c.52-1.06,2.21-5.1-.86-7.61A.5.5,0,0,0,21,1.75C19,1.75,16.51,4.2,15.74,5A14.61,14.61,0,0,0,8.3,5C7.51,4.19,5,1.75,3.08,1.75a.5.5,0,0,0-.32.11C.12,4,.92,7.52,1.89,9.44a9.41,9.41,0,0,0-1.31,5.8c.26,3.31,3,7,6.48,7h9.89c3.5,0,6.23-3.69,6.48-7A9.4,9.4,0,0,0,22.13,9.48ZM16,19.75H8a3.5,3.5,0,0,1-3.5-3.5c0-5.91,4.13-3.53,7.5-3.53s7.5-2.44,7.5,3.53A3.5,3.5,0,0,1,16,19.75Z"/><ellipse cx="15.52" cy="16.25" rx="1" ry="1.5"/><ellipse cx="8.52" cy="16.25" rx="1" ry="1.5"/></svg>
-            <a class="ml-2 text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/laravel-model-config">laravel-model-config</a>
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/laravel-custom-relation">laravel-custom-relation</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2016</span>
+            <span class="bg-orange-100 text-orange-800 p-1 rounded text-xs">Archived</span>
         </div>
-        <p class="text-gray-700">A config() for your laravel models</p>
-        <ul class="w-full list-none mt-3 flex flex-wrap items-center">
-            <li class="inline-flex items-center mr-4">
+        <p class="text-gray-700">A custom relation for when stock relations aren't enough</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
                 <span class="inline-block rounded-full bg-purple-400 light:bg-purple-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="http://php.net">php</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/winston">winston</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2015</span>
+            <span class="bg-orange-100 text-orange-800 p-1 rounded text-xs">Archived</span>
+        </div>
+        <p class="text-gray-700">Your personal valet for the web.</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
+                <span class="inline-block rounded-full bg-blue-400 light:bg-blue-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://reactjs.org/">react</a>
+            </li>
+            <li class="inline-flex items-center">
+                <span class="inline-block rounded-full bg-yellow-400 light:bg-yellow-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://reactjs.org/">chrome extension</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/reform">reform</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2011</span>
+            <span class="bg-orange-100 text-orange-800 p-1 rounded text-xs">Archived</span>
+        </div>
+        <p class="text-gray-700">Reform is an elegant and lightweight group of objects to help you create dynamic forms easier.</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
+                <span class="inline-block rounded-full bg-purple-400 light:bg-purple-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="http://php.net">php</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/revolver">revolver</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2011</span>
+            <span class="bg-orange-100 text-orange-800 p-1 rounded text-xs">Archived</span>
+        </div>
+        <p class="text-gray-700">A javascript library for building your own content slider</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
+                <span class="inline-block rounded-full bg-yellow-800 light:bg-yellow-800 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://coffeescript.org/">coffeescript</a>
             </li>
         </ul>
     </div>
