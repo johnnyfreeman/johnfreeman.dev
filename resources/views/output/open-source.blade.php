0d04ef3c7fb517866653ea559b7fd94f1f55cf44
John Freeman <prsjohnny@gmail.com>
2021-08-14 23:57:11 -0400

    added stimulus-clipboard library

--- resources/views/output/open-source.blade.php
+++ resources/views/output/open-source.blade.php
@@ -6,6 +6,20 @@
     <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
         <div class="flex items-center space-x-3">
             <x-icons.github class="inline-block fill-current w-4 h-4" />
+            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/stimulus-clipboard">stimulus-clipboard</a>
+            <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2021</span>
+        </div>
+        <p class="text-gray-700">A Stimulus controller for copy-to-clipboard functionality</p>
+        <ul class="w-full list-none mt-3 flex flex-wrap items-center space-x-4">
+            <li class="inline-flex items-center">
+                <span class="inline-block rounded-full bg-yellow-400 light:bg-yellow-600 h-3 w-3 mr-2"></span> <a class="hover:underline" href="https://github.com/stimulusjs/stimulus">stimulus</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans">
+        <div class="flex items-center space-x-3">
+            <x-icons.github class="inline-block fill-current w-4 h-4" />
             <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="https://github.com/johnnyfreeman/johnfreeman.dev">johnfreeman.dev</a>
             <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">2020</span>
         </div>
