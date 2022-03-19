4d9f63c83cd47b02c84a8bde4e1a9b69e775d6e2
John Freeman <prsjohnny@gmail.com>
2022-03-19 21:38:34 +0000

    updated about me command output

--- app/Myself.php
+++ app/Myself.php
@@ -3,16 +3,26 @@
 namespace App;
 
 use App\Auth\Root;
+use Carbon\Carbon;
 
 final class Myself extends Root
 {
     public $name = 'John Freeman';
-    public $nickname = 'Johnny';
+    public $nickname = 'John';
     public $email = 'john@johnfreeman.dev';
-    public $dob = '1985-09-07';
 
-    public function age()
+    public function birth(): Carbon
     {
-        return now()->diffInYears($this->dob);
+        return new Carbon('1985-09-07');
+    }
+
+    public function rvLife(): Carbon
+    {
+        return new Carbon('2018-07-01');
+    }
+
+    public function codingPassion(): Carbon
+    {
+        return new Carbon('1999-01-01');
     }
 }
