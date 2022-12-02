b37ad64c2672478c9febe537765af17da9201613
John Freeman <prsjohnny@gmail.com>
2022-12-02 02:05:51 -0500

    updated link in intro

--- resources/views/output/intro.blade.php
+++ resources/views/output/intro.blade.php
@@ -6,7 +6,7 @@
     </p>
     @else
     <p class="text-xl">
-        👋 What<span class="text-red-400">'</span>s up<span class="text-red-400">!</span> <strong>I<span class="text-red-400">'</span>m {{ $my->nickname }}</strong><span class="text-red-400">.</span><br>I build things<span class="text-red-400">.</span> And I write about<br>system design at <a href="https://www.eloquentarchitecture.com" class="text-gray-200 underline hover:text-white">eloquentarchitecture.com</a><span class="text-red-400">.</span>
+        👋 What<span class="text-red-400">'</span>s up<span class="text-red-400">!</span> <strong>I<span class="text-red-400">'</span>m {{ $my->nickname }}</strong><span class="text-red-400">.</span><br>I build things<span class="text-red-400">.</span> And I write about<br>system design at <a href="https://www.eloquentarchitecture.com" target="_blank" class="text-gray-200 underline hover:text-white">eloquentarchitecture.com</a><span class="text-red-400">.</span>
     </p>
     @endauth
 </x-output>
