8ef1a7ce476be7877f6d3e4eae83606990476484
John Freeman <prsjohnny@gmail.com>
2022-12-02 01:44:30 -0500

    blog links now open in new tab. use accent color of ghost tags

--- resources/views/output/blog.blade.php
+++ resources/views/output/blog.blade.php
@@ -3,9 +3,13 @@
 
     @foreach($posts as $post)
     <div class="mt-6 w-full max-w-xl bg-white text-gray-600 rounded-lg shadow p-6 font-sans space-y-3">
-        <div class="flex items-center space-x-3">
-            <x-icons.rss class="inline-block fill-current w-4 h-4" />
-            <a class="text-indigo-600 hover:text-indigo-700 hover:underline font-bold" href="{{ $post['url'] }}">{{ $post['title'] }}</a>
+        <div class="flex items-center justify-between">
+            <a href="{{ $post['url'] }}" target="_blank" class="group flex items-center space-x-2">
+                <x-icons.rss class="inline-block fill-current w-4 h-4" />
+                <span class="text-indigo-600 group-hover:text-indigo-700 font-bold">{{ $post['title'] }}</span>
+                <x-icons.arrow-up-right-from-square class="inline-block text-gray-400 fill-current w-3 h-3" />
+            </a>
+
             <span class="bg-gray-100 text-gray-800 p-1 rounded text-xs">
                 {{ (new Carbon\Carbon($post['published_at']))->shortAbsoluteDiffForHumans() }}
             </span>
@@ -16,7 +20,7 @@
         <ul class="w-full list-none flex flex-wrap items-center space-x-4">
             @foreach($post['tags'] as $tag)
             <li class="inline-flex items-center">
-                <span class="inline-block rounded-full bg-red-400 light:bg-red-600 h-3 w-3 mr-2"></span>
+                <span class="inline-block rounded-full h-3 w-3 mr-2" style="background-color:{{ $tag['accent_color'] }};"></span>
                 <a class="hover:underline" href="{{ $tag['url'] }}">{{ $tag['name'] }}</a>
             </li>
             @endforeach
