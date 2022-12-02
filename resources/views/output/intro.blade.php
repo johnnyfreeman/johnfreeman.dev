4c7d9dfa19066504faf8a26215f5e88993b98b8b
John Freeman <prsjohnny@gmail.com>
2022-12-02 02:03:38 -0500

    updated intro

--- resources/views/output/intro.blade.php
+++ resources/views/output/intro.blade.php
@@ -2,11 +2,11 @@
 <x-output input="intro" cache>
     @auth
     <p class="text-xl">
-        👋 Welcome back<span class="text-red-400">,</span> {{ $my->nickname }}<span class="text-red-400">!</span><br>made anything awesome lately<span class="text-red-400">?</span>
+        👋 <strong>Welcome back</strong><span class="text-red-400">,</span> {{ $my->nickname }}<span class="text-red-400">!</span><br>made anything awesome lately<span class="text-red-400">?</span>
     </p>
     @else
     <p class="text-xl">
-        👋 What<span class="text-red-400">'</span>s up<span class="text-red-400">!</span> <strong>I<span class="text-red-400">'</span>m {{ $my->nickname }}</strong><span class="text-red-400">,</span><br>a details<span class="text-red-400">-</span>oriented<span class="text-red-400">,</span> maker of things<span class="text-red-400">.</span>
+        👋 What<span class="text-red-400">'</span>s up<span class="text-red-400">!</span> <strong>I<span class="text-red-400">'</span>m {{ $my->nickname }}</strong><span class="text-red-400">.</span><br>I build things<span class="text-red-400">.</span> And I write about<br>system design at <a href="https://www.eloquentarchitecture.com" class="text-gray-200 underline hover:text-white">eloquentarchitecture.com</a><span class="text-red-400">.</span>
     </p>
     @endauth
 </x-output>
