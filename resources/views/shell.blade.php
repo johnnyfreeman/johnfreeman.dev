e0d54e5e29055d59c75c8ef31dfe9faa0d36c004
John Freeman <prsjohnny@gmail.com>
2023-02-27 03:02:03 -0500

    remove hint

--- resources/views/shell.blade.php
+++ resources/views/shell.blade.php
@@ -56,8 +56,6 @@
 
         <main class="flex-1" data-terminal-target="outputContainer" id="output">
             @yield('output')
-
-            <p class="mt-8 py-1 px-2 bg-gray-900 rounded italic text-gray-400 text-xs leading-loose">For more information, hit <kbd class="border border-indigo-600 light:border-indigo-300 uppercase tracking-wide p-1 rounded">/</kbd>, type <a data-turbo-stream class="bg-gray-900 text-teal-400 light:bg-gray-100 light:text-teal-600 p-1" href="{{ url('help') }}" data-terminal-input="help">help</a>, then hit <kbd class="border border-indigo-600 light:border-indigo-300 uppercase tracking-wide p-1 rounded">Enter</kbd><span class="text-red-400">.</span></p>
         </main>
 
         <footer class="sticky bottom-0 flex items-center justify-between bg-gray-900 light:bg-gray-100 uppercase text-xs text-gray-700 light:text-gray-500 py-2 -mx-5 md:-mx-10 px-5 md:px-10" data-controller="status">
